a809b7ae2ff5badbf14a7be78cd5feaf5f57c711
lipkerton <petr.petuxof@yandex.ru>
2026-06-10 09:17:08 +0300

    new test

--- file_test.go
+++ file_test.go
@@ -25,6 +25,27 @@ func readEmpty(fn *os.File) bool {
 	return sc.Text() == ""
 }
 
+func readEqual(ifl *os.File, ofl *os.File) bool {
+	sci := bufio.NewScanner(ifl)
+	for flag, tki := true, sci.Scan(); tki; flag, tki = true, sci.Scan() {
+		sco := bufio.NewScanner(ofl)
+		for _, v := range strings.Fields(sci.Text()) {
+			switch v {
+			case "error":
+				flag = false
+				break
+			}
+		}
+		if !flag {
+			continue
+		}
+		if sci.Text() != sco.Text() {
+			return false
+		}
+	}
+	return true
+}
+
 func TestFileContent(t *testing.T) {
 	input_file, err := os.CreateTemp("", "input.txt")
 	if err != nil {
@@ -60,5 +81,24 @@ func TestFileEmpty(t *testing.T) {
 	parseFile(input_file, output_file)
 	if !readEmpty(output_file) {
 		t.Errorf("`output.txt` is empty!")
+	}
+}
+
+func TestStringsIncomparable(t *testing.T) {
+	input_file, err := os.CreateTemp("", "input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	output_file, err := os.CreateTemp("", "output.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer input_file.Close()
+	defer output_file.Close()
+	parseFile(input_file, output_file)
+	if !readEmpty(output_file) {
+		t.Errorf("strings in files are incomparable")
 	}
 }
